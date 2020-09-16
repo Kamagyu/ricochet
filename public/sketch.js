1cb5431d6aada12e7f95123d2b0a160f5484025a
davawen <56738724+davawen@users.noreply.github.com>
2020-09-16 19:12:01 +0200

    Added title screen

--- public/sketch.js
+++ public/sketch.js
@@ -2,12 +2,39 @@
 
 let socket;
 
+let state = 0;
+let username = "";
+
+/** @type {p5.Element} */
+let nameInput;
+
+/** @type {p5.Element} */
+let playButton;
+
+let cnv;
+
 function setup()
 {
+	cnv = createCanvas(windowWidth, windowHeight);
+	
+	nameInput = createInput(username);
+	nameInput.attribute('name', 'bla');
+	nameInput.position(width/2 - 100, height/2 - 40);
+	
+	playButton = createButton("Play")
+	playButton.class("button");
+	playButton.position(width / 2 - 50, height/2 + 20);
+	
 	socket = io.connect();
 }
 
 function draw()
 {
+	background(230);
 	
+	noStroke();
+	fill(255);
+	
+	rectMode(CENTER);
+	rect(width/2, height/2, 300, 150);
 }
